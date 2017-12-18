e46ce09f0ee9eff277b551cde089a5ab2d27c5cb
Connor Bryan <cchromium@gmail.com>
2017-12-17 18:31:39 -0600

    Move Upgrade forms back to relevant slices vs. account

--- src/formscreens/index.js
+++ src/formscreens/index.js
@@ -1,62 +1,77 @@
 import config from "../config";
 import withPageHeader from "../providers/withPageHeader";
 
+// Account
 import SigninForm from "../slices/account/forms/Signin";
 import SignupForm from "../slices/account/forms/Signup";
 import UpdatePasswordForm from "../slices/account/forms/UpdatePassword";
 
-import UpgradeToShopForm from "../slices/account/forms/UpgradeToShop";
+// Shops
+import UpgradeToShopForm from "../slices/shops/forms/UpgradeToShop";
 import UpdateShopInfoForm from "../slices/shops/forms/UpdateShopInfo";
 
-import UpgradeToArtistForm from "../slices/account/forms/UpgradeToArtist";
+// Artists
+import UpgradeToArtistForm from "../slices/artists/forms/UpgradeToArtist";
 import UpdateArtistInfoForm from "../slices/artists/forms/UpdateArtistInfo";
 
-import UpgradeToBrandForm from "../slices/account/forms/UpgradeToBrand";
+// Brands
+import UpgradeToBrandForm from "../slices/brands/forms/UpgradeToBrand";
 import UpdateBrandInfoForm from "../slices/brands/forms/UpdateBrandInfo";
 import UploadCatalogForm from "../slices/brands/forms/UploadCatalog";
 
+// Pieces
 import UploadPieceForm from "../slices/pieces/forms/UploadPiece";
 
+// Account
 export const Signin = withPageHeader(config.pageHeaders.signin, SigninForm);
+
 export const Signup = withPageHeader(config.pageHeaders.signup, SignupForm);
+
 export const UpdatePassword = withPageHeader(
   config.pageHeaders.updatePassword,
   UpdatePasswordForm
 );
 
+// Shops
 export const BecomeAShop = withPageHeader(
   config.pageHeaders.becomeAShop,
   UpgradeToShopForm
 );
+
 export const UpdateShopInfo = withPageHeader(
   config.pageHeaders.updateShopInfo,
   UpdateShopInfoForm
 );
 
+// Artists
 export const BecomeAnArtist = withPageHeader(
   config.pageHeaders.becomeAnArtist,
   UpgradeToArtistForm
 );
+
 export const UpdateArtistInfo = withPageHeader(
   config.pageHeaders.updateArtistInfo,
   UpdateArtistInfoForm
 );
 
+// Brands
 export const BecomeABrand = withPageHeader(
   config.pageHeaders.becomeABrand,
   UpgradeToBrandForm
 );
+
 export const UpdateBrandInfo = withPageHeader(
   config.pageHeaders.updateBrandInfo,
   UpdateBrandInfoForm
 );
 
-export const UploadPiece = withPageHeader(
-  config.pageHeaders.uploadPiece,
-  UploadPieceForm
-);
-
 export const UploadCatalog = withPageHeader(
   config.pageHeaders.uploadCatalog,
   UploadCatalogForm
+);
+
+// Pieces
+export const UploadPiece = withPageHeader(
+  config.pageHeaders.uploadPiece,
+  UploadPieceForm
 );
