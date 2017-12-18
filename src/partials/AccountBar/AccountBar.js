3cbca56e46f9f3e848da6b46ffdb4db8b36c6bcd
Connor Bryan <cchromium@gmail.com>
2017-12-17 19:27:59 -0600

    Add "active" status to account bar items

--- src/partials/AccountBar/AccountBar.js
+++ src/partials/AccountBar/AccountBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Menu, Icon } from "semantic-ui-react";
 
 function AccountBar(props) {
@@ -9,13 +9,25 @@ function AccountBar(props) {
         <Menu.Item>
           <em>Signed in as Connor Bryan</em>
         </Menu.Item>
-        <Menu.Item as={Link} to="/sign-in">
+        <Menu.Item
+          as={Link}
+          to="/sign-in"
+          active={props.location.pathname === "/sign-in"}
+        >
           <Icon name="sign in" /> Sign in
         </Menu.Item>
-        <Menu.Item as={Link} to="/sign-up">
+        <Menu.Item
+          as={Link}
+          to="/sign-up"
+          active={props.location.pathname === "/sign-up"}
+        >
           <Icon name="user plus" /> Sign up
         </Menu.Item>
-        <Menu.Item as={Link} to="/my-account">
+        <Menu.Item
+          as={Link}
+          to="/my-account"
+          active={props.location.pathname === "/my-account"}
+        >
           <Icon name="settings" /> My account
         </Menu.Item>
         <Menu.Item as={Link} to="/">
@@ -26,4 +38,4 @@ function AccountBar(props) {
   );
 }
 
-export default AccountBar;
+export default withRouter(AccountBar);
