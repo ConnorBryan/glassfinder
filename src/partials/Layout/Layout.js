4fb189da23c008ca9212d8a5810649082623fb1a
Connor Bryan <cchromium@gmail.com>
2018-01-05 22:51:20 -0600

    Add the splash to the Layout

--- src/partials/Layout/Layout.js
+++ src/partials/Layout/Layout.js
@@ -6,16 +6,17 @@ import {
   Button,
   Icon,
   Responsive,
-  Image
+  Image,
+  Header
 } from "semantic-ui-react";
+import { Parallax } from "react-parallax";
 import styled from "styled-components";
-import Aux from "react-aux";
 
 function MobileNavbar(props) {
   return (
     <Menu widths={2} borderless secondary>
       <Menu.Item onClick={() => {}}>
-        <Image src="https://placehold.it/150x30" />
+        <Image size="small" src="/logo.png" />
       </Menu.Item>
       <Menu.Item
         as={HamburgerButton}
@@ -32,7 +33,7 @@ function DesktopNavbar(props) {
   return (
     <Menu borderless secondary>
       <Menu.Item onClick={() => {}}>
-        <Image src="https://placehold.it/300x40" />
+        <Image size="small" src="/logo.png" />
       </Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item onClick={() => {}}>Link A</Menu.Item>
@@ -61,13 +62,40 @@ function Navbar(props) {
   );
 }
 
+function Splash(props) {
+  return (
+    <Parallax bgImage="/city.jpg" strength={400} basic>
+      <Container>
+        <Splasher>
+          <Segment as={Ghost} padded="very" textAlign="center" basic>
+            <Header as={Tagline}>
+              Paraphernalia, <Emphasized>revolutionized.</Emphasized>
+            </Header>
+            <Header
+              as={Subtagline}
+              content="Welcome to the new way of lighting up."
+            />
+            <Button.Group as={GetStarted}>
+              <Button basic primary>
+                Get started <Icon name="send" />
+              </Button>
+            </Button.Group>
+          </Segment>
+        </Splasher>
+      </Container>
+    </Parallax>
+  );
+}
+
 function Layout(props) {
   return (
     <Container as={Segment.Group} fluid>
       <Segment attached="top" basic>
         <Navbar />
       </Segment>
-      <Segment attached="bottom" basic />
+      <Segment as={NoPadding} attached="bottom" basic>
+        <Splash />
+      </Segment>
     </Container>
   );
 }
@@ -86,3 +114,20 @@ const GetStarted = styled.div`
     text-transform: uppercase !important;
   }
 `;
+
+const Splasher = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh !important;
+`;
+
+const NoPadding = styled.div`padding: 0 !important;`;
+
+const Tagline = styled.div`font-size: 3rem !important;`;
+
+const Subtagline = styled.div`font-size: 1.5rem !important;`;
+
+const Ghost = styled.div`background: rgba(255, 255, 255, 0.5) !important;`;
+
+const Emphasized = styled.em`color: #2185d0 !important;`;
