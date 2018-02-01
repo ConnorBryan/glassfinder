5158a458b8025fc1694100aede480bd34f5e4d7f
Connor Bryan <cchromium@gmail.com>
2018-01-31 19:13:10 -0600

    Responsive notifications.

--- src/client/components/Notification/Notification.js
+++ src/client/components/Notification/Notification.js
@@ -1,32 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { TransitionablePortal, Label, Segment } from "semantic-ui-react";
+import {
+  TransitionablePortal,
+  Responsive,
+  Label,
+  Segment
+} from "semantic-ui-react";
+import Aux from "react-aux";
 
 function Notification({ dismiss, message }) {
-  return (
-    <TransitionablePortal open>
-      <Segment
-        color="blue"
-        style={{
-          position: "fixed",
-          zIndex: 1000,
-          width: "40%",
-          left: "30%",
-          top: "10%"
+  const interior = (
+    <Aux>
+      <Label
+        {...{
+          as: "a",
+          color: "blue",
+          corner: "right",
+          icon: "close",
+          onClick: dismiss
         }}
+      />
+      <span style={{ fontSize: "1.2rem" }}>{message}</span>
+    </Aux>
+  );
+
+  return (
+    <Aux>
+      <Responsive
+        as={TransitionablePortal}
+        maxWidth={Responsive.onlyMobile.maxWidth}
+        open
       >
-        <Label
-          {...{
-            as: "a",
-            color: "blue",
-            corner: "right",
-            icon: "close",
-            onClick: dismiss
+        <Segment
+          color="blue"
+          style={{
+            position: "fixed",
+            zIndex: 1000,
+            width: "80%",
+            left: "10%",
+            top: "10%"
           }}
+          content={interior}
         />
-        <span style={{ fontSize: "1.2rem" }}>{message}</span>
-      </Segment>
-    </TransitionablePortal>
+      </Responsive>
+      <Responsive
+        as={TransitionablePortal}
+        minWidth={Responsive.onlyTablet.minWidth}
+        open
+      >
+        <Segment
+          color="blue"
+          style={{
+            position: "fixed",
+            zIndex: 1000,
+            width: "40%",
+            left: "30%",
+            top: "10%"
+          }}
+          content={interior}
+        />
+      </Responsive>
+    </Aux>
   );
 }
 
