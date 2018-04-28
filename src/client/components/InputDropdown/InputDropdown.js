63b40d4470bde4831653604fd9dd6b6825318c31
Connor Bryan <connorbryan@Connors-MacBook-Pro.local>
2018-04-28 04:00:27 -0500

    Make a badass input dropdown for multiuse

---
+++ src/client/components/InputDropdown/InputDropdown.js
@@ -0,0 +1,254 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Button, Loader } from "semantic-ui-react";
+
+const Styles = styled.div`
+  .wrapper {
+    position: relative;
+    width: ${props => props.width};
+    height: ${props => props.height};
+    display: flex;
+    align-items: center;
+
+    > div {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  input {
+    width: 100%;
+    height: 100%;
+    border: 1px solid white;
+    background: black;
+    color: white;
+    font-size: 1.66rem;
+    letter-spacing: 0.15rem;
+    padding: 1rem;
+  }
+
+  button {
+    width: 5rem !important;
+    height: ${props => `${props.height} !important`};
+    border: 1px solid white !important;
+    border-left: none !important;
+  }
+
+  .loading-dropdown {
+    position: absolute;
+    top: ${props => props.height};
+    width: 100%;
+    border: 1px solid #ccc;
+    background: black;
+    color: white;
+    height: 115px;
+  }
+
+  ul {
+    position: absolute;
+    top: ${props => props.height};
+    width: 100%;
+    list-style-type: none;
+    border: 1px solid #ccc;
+    background: black;
+    color: white;
+    margin: 0;
+    padding: 0;
+    max-height: 230px;
+    overflow-y: auto;
+    z-index: 1;
+
+    li {
+      margin: 0;
+      padding: 2rem;
+      transition: background 0.33s ease-in;
+      font-size: 1.3rem;
+      border-bottom: 1px solid rgba(45, 45, 45, 0.6);
+
+      &:hover {
+        background: #222;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export default class InputDropdown extends Component {
+  static propTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string,
+    minimumCharactersForDropdown: PropTypes.number,
+    valueProperty: PropTypes.string,
+    labelProperty: PropTypes.string,
+    placeholder: PropTypes.string,
+    filtered: PropTypes.bool,
+    service: PropTypes.func,
+    onSubmit: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    width: "40rem",
+    height: "5rem",
+    minimumCharactersForDropdown: 2,
+    valueProperty: "id",
+    labelProperty: "name",
+    placeholder: "",
+    filtered: true,
+    service: () => Promise.resolve([])
+  };
+
+  state = {
+    value: "",
+    dropdownVisible: false,
+    isLoading: true,
+    isFocused: false,
+    collection: []
+  };
+
+  input = null;
+
+  componentDidMount() {
+    this.loadCollection();
+  }
+
+  componentDidUpdate() {
+    this.checkDropdownStatus();
+  }
+
+  setValue = (value, submitAfterSetting = false) =>
+    this.setState({ value }, () => submitAfterSetting && this.handleSubmit());
+
+  focus = () => this.setState({ isFocused: true });
+
+  blur = () => this.setState({ isFocused: false });
+
+  revealDropdown = () => this.setState({ dropdownVisible: true });
+
+  hideDropdown = () => this.setState({ dropdownVisible: false });
+
+  handleInputChange = ({ target: { value } }) => this.setValue(value);
+
+  handleInputFocus = () => setTimeout(this.focus, 200);
+
+  handleInputBlur = () => setTimeout(this.blur, 200);
+
+  handleInputKeydown = ({ key }) => {
+    const { isFocused } = this.state;
+
+    if (key === "Enter") {
+      this.blur();
+      this.handleSubmit();
+    }
+
+    if (!isFocused) {
+      this.focus();
+    }
+  };
+
+  handleSubmit = () => {
+    const { onSubmit } = this.props;
+    const { value: label } = this.state;
+    const labelToValueDictionary = this.getLabelToValueDictionary();
+    const valueToSubmit = labelToValueDictionary[label];
+
+    if (typeof valueToSubmit === "undefined") {
+      return;
+    }
+
+    onSubmit(valueToSubmit, label);
+  };
+
+  checkDropdownStatus = () => {
+    const { minimumCharactersForDropdown } = this.props;
+    const { value, dropdownVisible } = this.state;
+    const dropdownShouldBeVisible =
+      value.length >= minimumCharactersForDropdown;
+
+    if (dropdownShouldBeVisible && !dropdownVisible) {
+      this.revealDropdown();
+    } else if (!dropdownShouldBeVisible && dropdownVisible) {
+      this.hideDropdown();
+    }
+  };
+
+  loadCollection = async () => {
+    const { service } = this.props;
+    const collection = await service();
+
+    this.setState({
+      collection,
+      isLoading: false
+    });
+  };
+
+  renderDropdownItems = () =>
+    (this.props.filtered
+      ? this.getFilteredCollection()
+      : this.state.collection
+    ).map((item, index) => (
+      <li
+        key={index}
+        onClick={() => this.setValue(item[this.props.labelProperty], true)}
+      >
+        {item[this.props.labelProperty]}
+      </li>
+    ));
+
+  getFilteredCollection = () =>
+    this.state.collection.filter(item =>
+      item[this.props.labelProperty].includes(this.state.value)
+    );
+
+  getLabelToValueDictionary = () =>
+    this.state.collection.reduce((prev, next) => {
+      const { labelProperty, valueProperty } = this.props;
+
+      prev[next[labelProperty]] = next[valueProperty];
+
+      return prev;
+    }, Object.create(null));
+
+  render() {
+    const { service, onSubmit, placeholder, ...styles } = this.props;
+    const {
+      value,
+      isLoading,
+      isFocused,
+      dropdownVisible,
+      collection
+    } = this.state;
+    const dropdownContent = isLoading ? (
+      <section className="loading-dropdown">
+        <Loader active />
+      </section>
+    ) : (
+      <ul>{this.renderDropdownItems()}</ul>
+    );
+
+    return (
+      <Styles {...styles}>
+        <div className="wrapper">
+          <div>
+            <input
+              ref={node => (this.input = node)}
+              type="text"
+              placeholder={placeholder}
+              value={value}
+              onChange={this.handleInputChange}
+              onFocus={this.handleInputFocus}
+              onBlur={this.handleInputBlur}
+              onKeyDown={this.handleInputKeydown}
+              onClick={this.handleInputFocus}
+            />
+            {isFocused &&
+              dropdownVisible &&
+              collection.length > 0 &&
+              dropdownContent}
+          </div>
+          <Button secondary icon="chevron right" onClick={this.handleSubmit} />
+        </div>
+      </Styles>
+    );
+  }
+}
--- src/client/myAccount/screens/UpdateAssociations/UpdateAssociations.js
+++ src/client/myAccount/screens/UpdateAssociations/UpdateAssociations.js
@@ -1,67 +1,56 @@
 import React, { Component } from "react";
 import { Formik, Field } from "formik";
-import { Loader } from "semantic-ui-react";
+import { Loader, Segment } from "semantic-ui-react";
 import styled from "styled-components";
 
 import API from "../../../services";
+import InputDropdown from "../../../components/InputDropdown";
 import { InputWithDropdown } from "../_UploadPiece";
 
 const BRAND_ID_TO_NAME = {};
 
 const Styles = styled.div`
   color: white;
+
+  h2 {
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+  }
+
+  .dropdown-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .dropdown-segment {
+    margin: 0 !important;
+    padding: 2rem !important;
+    max-width: 50rem !important;
+    font-size: 1.2rem !important;
+
+    p {
+      height: 5rem !important;
+    }
+  }
 `;
 
 export default class UpdateAssociations extends Component {
-  state = {
-    loading: true,
-    associatedBrands: [],
-    allBrands: []
-  };
-
-  componentDidMount() {
-    this.setAssociatedBrands();
-    this.setAllBrands();
-  }
-
-  setAssociatedBrands = async () => {
+  handleAssociateBrandChange = async (value, label) => {
     const {
       account: {
         link: { id }
       }
     } = this.props;
 
-    const associatedBrands = await API.getShopToBrandAssociations(id);
-
-    this.setState({
-      loading: false,
-      associatedBrands
-    });
-  };
-
-  setAllBrands = async () => {
-    const allBrands = await API.retrieveAllBrands();
-
-    this.setState({ allBrands });
-  };
-
-  handleAssociateBrandChange = async ({ target: { value } }) => {
-    const {
-      account: {
-        link: { id }
-      }
-    } = this.props;
-
-    if (window.confirm(`Associate with ${BRAND_ID_TO_NAME[value]}?`)) {
+    if (window.confirm(`Associate with ${label}?`)) {
       const association = await API.associateShopWithBrand(id, value);
 
       window.location.reload();
-    } else {
-      this.associateSelect.value = "Select a brand to associate with it.";
     }
   };
 
-  handleDisassociateBrandChange = async ({ target: { value } }) => {
+  handleDissociateBrandChange = async (value, label) => {
     const {
       account: {
         link: { id }
@@ -70,72 +59,57 @@ export default class UpdateAssociations extends Component {
 
     if (!value) return;
 
-    if (window.confirm(`Disassociate with ${BRAND_ID_TO_NAME[value]}?`)) {
+    if (window.confirm(`Disassociate with ${label}?`)) {
       await API.disassociateShopWithBrand(id, value);
 
       window.location.reload();
-    } else {
-      this.dissociateSelect.value =
-        "Select an associated brand to disassociate with it.";
     }
   };
 
-  filter = brands => {
-    const { associatedBrands } = this.state;
-    const existingIds = associatedBrands.reduce((prev, next) => {
-      return { ...prev, [next.id]: true };
-    }, {});
-
-    return brands.filter(brand => !existingIds[brand.id]);
-  };
-
   render() {
-    const { loading, associatedBrands, allBrands } = this.state;
-
-    if (loading) return <Loader>Loading...</Loader>;
+    const {
+      account: {
+        link: { id }
+      }
+    } = this.props;
+    const associatedBrandService = API.getShopToBrandAssociations.bind(
+      null,
+      id
+    );
 
     return (
       <Styles>
-        <p>Associated brands</p>
-        <select
-          ref={node => (this.dissociateSelect = node)}
-          name="brand"
-          onChange={this.handleDisassociateBrandChange}
-        >
-          <option name="brand" value={null}>
-            Select an associated brand to disassociate with it.
-          </option>
-          {associatedBrands.map((brand, index) => {
-            BRAND_ID_TO_NAME[brand.id] = brand.name;
-
-            return (
-              <option key={index} name="brand" value={brand.id}>
-                {brand.name}
-              </option>
-            );
-          })}
-        </select>
-
-        <p>Associate with a brand</p>
-        <select
-          ref={node => (this.associateSelect = node)}
-          name="associateBrand"
-          onChange={this.handleAssociateBrandChange}
-        >
-          <option name="associateBrand" value={null}>
-            Select a brand to associate with it.
-          </option>
-          {this.filter(allBrands).map((brand, index) => {
-            // Use dictionary for handling the change.
-            BRAND_ID_TO_NAME[brand.id] = brand.name;
-
-            return (
-              <option key={index} name="associateBrand" value={brand.id}>
-                {brand.name}
-              </option>
-            );
-          })}
-        </select>
+        <div className="dropdown-wrapper">
+          <Segment className="dropdown-segment" inverted>
+            <h2>Associate with a brand</h2>
+            <p>
+              This dropdown contains a list of all brands on Glassfinder. Select
+              a brand that your shop carries to ensure customers can find you on
+              the Shop view.
+            </p>
+            <InputDropdown
+              placeholder="Enter a brand to associate with it."
+              minimumCharactersForDropdown={0}
+              service={API.retrieveAllBrands}
+              onSubmit={this.handleAssociateBrandChange}
+            />
+          </Segment>
+          <Segment className="dropdown-segment" inverted>
+            <h2>Associated brands</h2>
+            <p>
+              This dropdown contains a list of all the brands your shop is
+              associated with. Select a brand from this list to remove that
+              association and thereby prevent your shop from showing up on the
+              map when it is filtered by that brand.
+            </p>
+            <InputDropdown
+              placeholder="Enter a brand to dissociate with it."
+              minimumCharactersForDropdown={0}
+              service={associatedBrandService}
+              onSubmit={this.handleDissociateBrandChange}
+            />
+          </Segment>
+        </div>
       </Styles>
     );
   }
